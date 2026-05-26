d4da0342c5e4c8673d207e6262b002b9ebb9f310
Duvard1 <duvardesteban@hotmail.com>
2026-05-26 17:23:48 -0500

    test 2 deploy qa

--- src/data/topics.js
+++ src/data/topics.js
@@ -1,7 +1,7 @@
 module.exports = [
   {
     id: 1,
-    title: "Antes del versionamiento xdddd",
+    title: "Antes del versionamiento :ccccc",
     description: "Código enviado por correo, USB o archivos duplicados. Problemas de sobrescritura y sin historial.",
     link: "https://git-scm.com/"
   },
